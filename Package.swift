c4735a4ae4180884b3a6d59d9dcc51d4fd781d8a
Taylor Lineman <taylor.lineman@gmail.com>
2024-05-17 18:14:54 -0400

    Added Dynamic Library

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "HydrogenReporter", targets: ["HydrogenReporter"])
+        .library(name: "HydrogenReporter", targets: ["HydrogenReporter"]),
+        .library(name: "DynamicHydrogenReporter", type: .dynamic, targets: ["HydrogenReporter"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
